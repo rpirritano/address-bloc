ac4cfc2334bc54975b24d820dac9a1fbfc0e77b7
Robert Pirritano <rpirritano@gmail.com>
2017-04-03 11:18:12 -0700

    Assignment 6 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ require_relative '../models/address_book'
      puts "2 - Create an entry"
      puts "3 - Search for an entry"
      puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "5 - View Entry Number n"
+     puts "6 - Exit"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -36,6 +37,10 @@ require_relative '../models/address_book'
         read_csv
         main_menu
       when 5
+        system "clear"
+        entry_n_submenu
+        main_menu
+      when 6
         puts "Good-bye!"
 
         exit(0)
@@ -48,6 +53,7 @@ require_relative '../models/address_book'
     end
 
     def view_all_entries
+      puts "Viewing all the entries!!!"
 
       address_book.entries.each do |entry|
         system "clear"
@@ -76,6 +82,20 @@ require_relative '../models/address_book'
 
       system "clear"
       puts "New entry created"
+    end
+
+    def entry_n_submenu
+      print "Entry number to view: "
+      selection = gets.chomp.to_i
+      if selection <= @address_book.entries.count
+        puts @address_book.entries[selection]
+        puts "Press enter to return to the main menu"
+        gets.chomp
+        system "clear"
+      else
+        puts "#{selection} is not a valid input"
+        entry_n_submenu
+      end
     end
 
     def search_entries
